fcd035c5d0e34020c7e9839750cf9e229b538ee1
extripador117 <tom_rugalhotmail.com>
2018-07-17 00:14:15 -0600

    avance a caja

--- src/assets/sources/js/content/caja.js
+++ src/assets/sources/js/content/caja.js
@@ -1,5 +1,41 @@
 
+$("#MontoInicialbtn").click(function(){
+var errors = [];
+    if($("#dineroEnCaja").val()==0)
+         errors.push('Es obligatorio ingresar el monton con el que se inicio el dia');
+    if(errors.length<=0){
+        $("#MontoInicialbtn").hide(500);
+        $( "#dineroEnCaja" ).prop( "disabled", true );
+        $("#DataAfterMonto").show(500);
+        $.ajax({ 
+            type: 'POST',   
+            url: window.location.origin+'/murilloFerreteria/src/index.php/' + 'Caja/setMontoInicial',   
+            data: {
+                caja:$("#dineroEnCaja").val(),
+                ingreso:1,
+                retiro:1,
+                empleado:1                
+             }
+            });
+    }
 
+
+});
+
+$("#addRetiros").click(function(){
+    var errors = [];
+        if($("#retirosDiarios").val()==0)
+        errors.push('Es obligatorio ingresar el retiro que obtuvo este dia');
+        $.ajax({ 
+            type: 'POST',   
+            url: window.location.origin+'/murilloFerreteria/src/index.php/' + 'Caja/setMontoInicial',   
+            data: {
+                ingreso:$("#retirosDiarios").val(),                  
+             }
+        });
+
+
+});
 $("#calcularMonto").click(function(){
 	var errors = [];
 	if($("#dineroEnCaja").val()==0)
@@ -20,12 +56,6 @@ $("#calcularMonto").click(function(){
         		empleado:1
         	}
     		});
-	  /*  getAjax('POST','Caja/setCaja',{
-	        'caja':$("#dineroEnCaja").val(),
-	        'ingreso':$("#ingresosDiarios").val(),
-	        'retiro':$("#retirosDiarios").val(),
-	        'empleado':1
-	    },'asda');*/
 	}else{
         cleanBotonesModal(true);
         modal('danger','large','ERROR',getErrors(errors),true);
