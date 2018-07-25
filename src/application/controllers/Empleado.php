3d29347a482b9e4de8e10e473ee0c3f39db8b232
WizzKing <cotafiesta@gmail.com>
2018-07-24 18:53:30 -0600

    back empleados

--- src/application/controllers/Empleado.php
+++ src/application/controllers/Empleado.php
@@ -17,4 +17,91 @@ class Empleado extends CI_Controller
 		$data['dataMenu'] = $this->M_panel->getMenu();
 		$this->load->view('Empleados',$data);
 	}
+
+	public function get()
+	{
+		$result = $this->M_empleado->getEmpleado();
+    	echo json_encode($result);
+	}
+
+	public function push()
+	{
+		$queryCliente = array(
+			'nombre' => $this->input->post('nombre'),
+			'a_p' => $this->input->post('a_p'),
+			'a_m' => $this->input->post('a_m'),
+			'domicilio' => $this->input->post('domicilio'), 
+		);
+
+		$queryTelefono = array(
+			'numero' => $this->input->post('numero'),
+			'tipo' => $this->input->post('tipo'),
+		);
+		$this->M_empleado->pushEmpleado($queryCliente,$queryTelefono);
+
+		echo '<form action="#" method="post">'
+	.'<div class="row">'
+	.'<div class="form-group col-md-12" style="text-align: center;">'
+	.'<h2>Accion completada.</h2>'
+	.'</div>'
+	.'</div>'
+	.'</form>';
+	}
+	public function update()
+	{
+		$cliente = $this->input->post('id');
+		$queryCliente = array(
+			'nombre' => $this->input->post('nombre'),
+			'a_p' => $this->input->post('a_p'),
+			'a_m' => $this->input->post('a_m'),
+			'domicilio' => $this->input->post('domicilio'), 
+		);
+
+		$queryTelefono = array(
+			'numero' => $this->input->post('numero'),
+			'tipo' => $this->input->post('tipo'),
+		);
+		$this->M_empleado->updateEmpleado($cliente,$queryCliente,$queryTelefono);
+
+		echo '<form action="#" method="post">'
+	.'<div class="row">'
+	.'<div class="form-group col-md-12" style="text-align: center;">'
+	.'<h2>Accion completada.</h2>'
+	.'</div>'
+	.'</div>'
+	.'</form>';
+	}
+	public function drop()
+	{
+		$this->M_empleado->dropEmpleado($this->input->post('id'));
+
+		echo '<form action="#" method="post">'
+	.'<div class="row">'
+	.'<div class="form-group col-md-12" style="text-align: center;">'
+	.'<h2>Accion completada.</h2>'
+	.'</div>'
+	.'</div>'
+	.'</form>';
+	}
+
+		public function search()
+	{
+		if ($this->input->post('status') == 1) {
+			$query = array(
+				'empleados.puntos' => $this->input->post('data')
+			);
+		}
+		if ($this->input->post('status') == 2) {
+			$query = array(
+				'empleados.nombre' => $this->input->post('data')
+			);
+		}
+		if ($this->input->post('status') == 3) {
+			$query = array(
+				'telefonos.numero' => $this->input->post('data')
+			);
+		}
+		$result = $this->M_empleado->getSearch($query);
+    	echo json_encode($result);
+	}
 }
